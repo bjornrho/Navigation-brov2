1521c6240711ec0a7b7e4528a63618582d329669
Bjornar <bjornar.rh@gmail.com>
2021-09-20 13:10:14 +0200

    added depth measurement

--- src/brov2_barometer/brov2_barometer/BarometerDataPublisher.py
+++ src/brov2_barometer/brov2_barometer/BarometerDataPublisher.py
@@ -16,6 +16,7 @@ class BarometerDataPublisher(Node):
         self.timer = self.create_timer(read_period, self.barometer_read_and_publish)
 
         self.sensor = ms5837.MS5837_30BA()
+        # self.sensor.setFluidDensity()
         if not self.sensor.init():
             print("Sensor could not be initialized")
             exit(1)
@@ -24,16 +25,21 @@ class BarometerDataPublisher(Node):
         msg = Barometer()
 
         if self.sensor.read():
-                msg.pressure_mbar           = self.sensor.pressure()                           # Default is mbar (no arguments)
-                msg.pressure_psi            = self.sensor.pressure(ms5837.UNITS_psi)           # Request psi
-                msg.temperature_celsius     = self.sensor.temperature()                        # Default is degrees C (no arguments)
-                msg.temperature_farenheit   = self.sensor.temperature(ms5837.UNITS_Farenheit)  # Request Farenheit
+                msg.depth                   = self.sensor.depth()                               # Depth in meters using the fluid density (kg/m^3) configured by setFluidDensity()
+                msg.pressure_mbar           = self.sensor.pressure()                            # Default is mbar (no arguments)
+                msg.pressure_psi            = self.sensor.pressure(ms5837.UNITS_psi)            # Request psi
+                msg.temperature_celsius     = self.sensor.temperature()                         # Default is degrees C (no arguments)
+                msg.temperature_farenheit   = self.sensor.temperature(ms5837.UNITS_Farenheit)   # Request Farenheit
         else:
                 print("Sensor read failed!")
                 exit(1)
 
         self.publisher_.publish(msg)
-        self.get_logger().info('P: %0.1f mbar  %0.3f psi\tT: %0.2f C  %0.2f F' % (msg.pressure_mbar, msg.pressure_psi, msg.temperature_celsius, msg))
+        self.get_logger().info('Depth: %0.2f m\tP: %0.1f mbar  %0.3f psi\tT: %0.2f C  %0.2f F' % (msg.depth, 
+                                                                                                  msg.pressure_mbar, 
+                                                                                                  msg.pressure_psi, 
+                                                                                                  msg.temperature_celsius, 
+                                                                                                  msg.temperature_farenheit))
 
 
 def main(args=None):

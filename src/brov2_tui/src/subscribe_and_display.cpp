8bd07751fbafa71ae679512c7fa2823c02de2221
Bjornar <bjornar.rh@gmail.com>
2021-09-27 22:15:19 +0200

    visual baseline, possibly working with publisher class

--- src/brov2_tui/src/subscribe_and_display.cpp
+++ src/brov2_tui/src/subscribe_and_display.cpp
@@ -1,10 +1,11 @@
 #include <functional>
 #include <memory>
-#include <ncurses.h>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "brov2_interfaces/msg/barometer.hpp"
+
+#include "brov2_tui/ncursesfun.h"
 
 using std::placeholders::_1;
 
@@ -12,7 +13,8 @@ class SensorSubscriber : public rclcpp::Node
 {
 public:
   SensorSubscriber()
-  : Node("sensor_subscriber")
+  : Node("sensor_subscriber"),
+  nf{}
   {
     // dvl_vel_subscription_ = this->create_subscription<std_msgs::msg::String>(
     //   "topic", 10, std::bind(&SensorSubscriber::dvl_vel_callback, this, _1));
@@ -20,9 +22,9 @@ public:
     //   "topic", 10, std::bind(&SensorSubscriber::dvl_pos_callback, this, _1));
     barometer_subscription_ = this->create_subscription<brov2_interfaces::msg::Barometer>(
       "barometer_data", 10, std::bind(&SensorSubscriber::barometer_callback, this, _1));
-    
-    initscr();			/* Start curses mode 		  */
-	endwin();			/* End curses mode		  */
+
+    wins = nf.InitWindows();
+    nf.SetupTui(wins);
   }
 
 private:
@@ -36,17 +38,17 @@ private:
   //   RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
   // }
 
-  void barometer_callback(const brov2_interfaces::msg::Barometer::ConstSharedPtr msg) const
+  void barometer_callback(const brov2_interfaces::msg::Barometer::ConstSharedPtr msg)
   {
-    // RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->depth);
-    printw("Hello World !!!");
-    
-	refresh();
+    nf.UpdateBarometerWindow(wins[3], msg);
   }
 
   // rclcpp::Subscription<std_msgs::msg::String>::SharedPtr dvl_vel_subscription_;
   // rclcpp::Subscription<std_msgs::msg::String>::SharedPtr dvl_pos_subscription_;
   rclcpp::Subscription<brov2_interfaces::msg::Barometer>::SharedPtr barometer_subscription_;
+  NcursesFunctions nf;
+  std::vector<WINDOW*> wins;
+
 };
 
 int main(int argc, char * argv[])
@@ -54,5 +56,17 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<SensorSubscriber>());
   rclcpp::shutdown();
+
+  // NcursesFunctions nf;
+  // std::vector<WINDOW*> my_wins = nf.InitWindows();
+  // 
+  // nf.SetupTui(my_wins);
+
+
+
+  getch();
+  endwin();
+
+
   return 0;
 }

471a3c0e72d6e48b271c03108bd8dd3b9fd44ba5
Bjornar <bjornar.rh@gmail.com>
2021-09-30 20:14:27 +0200

    launching multiple drivers

---
+++ launch/brov2_launch.py
@@ -0,0 +1,30 @@
+import launch
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        #launch.actions.ExecuteProcess(
+        #    cmd=['ros2', 'bag', 'record', '-a'],
+        #    output='screen'
+        #),
+        Node(
+            package='brov2_barometer',
+            namespace='barometer',
+            executable='barometer_publisher',
+            name='sensors'
+        ),
+        Node(
+            package='brov2_dvl',
+            namespace='dvl',
+            executable='DVLPublisher',
+            name='sensors'
+        ),
+        Node(
+            package='bno055_sensor',
+            namespace='bno055',
+            executable='bno055_sensor_node',
+            name='sensors'
+        )
+    ])

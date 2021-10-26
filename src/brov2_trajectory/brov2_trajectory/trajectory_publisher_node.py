a1ee88c3530ca2f7509b8fda7e500ceb2e7db514
Bjornar <bjornar.rh@gmail.com>
2021-10-26 13:24:10 +0200

    changed topic, changed Reference-msg, added all velocities, add print-statements

--- src/brov2_trajectory/brov2_trajectory/trajectory_publisher_node.py
+++ src/brov2_trajectory/brov2_trajectory/trajectory_publisher_node.py
@@ -1,7 +1,8 @@
 import csv
+import rclpy
 from rclpy.node import Node
 
-from brov2_interfaces.msg import Reference
+from bluerov_interfaces.msg import Reference
 from std_msgs.msg import Header
 from geometry_msgs.msg import Vector3, Quaternion, Twist
 
@@ -11,18 +12,21 @@ class TrajectoryPublisher(Node):
     def __init__(self):
         # Initialization of trajectory publisher
         super().__init__('trajectory_publisher')
-        self.publisher_ = self.create_publisher(Reference, 'trajectory_topic', 10)
+        self.publisher_ = self.create_publisher(Reference, '/CSE/references', 10)
         trajectory_period = 1/10  # seconds
         self.trajectory_timer = self.create_timer(trajectory_period, self.reference_publisher)
         self.trajectory_iterator = 0
 
         # Getting trajectories from csv-file
-        file = open('horizontal_trajectory.csv')
+        #file = open('horizontal_trajectory.csv')
+        file = open('pool_trajectory.csv')
         csvreader = csv.reader(file)
         self.rows = []
         for row in csvreader:
             self.rows.append([float(s) for s in row])
         file.close()
+
+        self.get_logger().info("Trajectory publisher initialized. Start publishing trajectory!")
 
     def reference_publisher(self):
         position = Vector3()
@@ -40,12 +44,17 @@ class TrajectoryPublisher(Node):
         velocity.linear.x = self.rows[self.trajectory_iterator][7]
         velocity.linear.y = self.rows[self.trajectory_iterator][8]
         velocity.linear.z = self.rows[self.trajectory_iterator][9]
+        velocity.angular.x = self.rows[self.trajectory_iterator][13]
+        velocity.angular.y = self.rows[self.trajectory_iterator][14]
+        velocity.angular.z = self.rows[self.trajectory_iterator][15]
 
-        # ASK ABOUT THE ACCELERATION. IS IT ANGULAR? IF NOT USE 10,11,12
         acceleration = Twist()
-        acceleration.angular.x = self.rows[self.trajectory_iterator][13]
-        acceleration.angular.y = self.rows[self.trajectory_iterator][14]
-        acceleration.angular.z = self.rows[self.trajectory_iterator][15]
+        acceleration.linear.x = self.rows[self.trajectory_iterator][10]
+        acceleration.linear.y = self.rows[self.trajectory_iterator][11]
+        acceleration.linear.z = self.rows[self.trajectory_iterator][12]
+        acceleration.angular.x = self.rows[self.trajectory_iterator][16]
+        acceleration.angular.y = self.rows[self.trajectory_iterator][17]
+        acceleration.angular.z = self.rows[self.trajectory_iterator][18]
 
         current_reference = Reference()
         current_reference.header = Header()
@@ -56,4 +65,10 @@ class TrajectoryPublisher(Node):
         current_reference.acceleration = acceleration
 
         self.publisher_.publish(current_reference)
-        self.trajectory_iterator += 1
+
+        if self.trajectory_iterator < len(self.rows)-1:
+            self.trajectory_iterator += 1
+        else:
+            self.get_logger().info("Trajectory completed!")
+            self.destroy_node()
+            self.get_logger().info("Node destroyed. Start new instance of reference publisher or switch to TeleOP!")

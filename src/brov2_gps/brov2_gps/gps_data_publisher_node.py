b49a6dd753f1ca75ebd15f919739c3b942159b0d
Bjornar <bjornar.rh@gmail.com>
2022-09-07 11:25:03 +0200

    Grouping parameters.

--- src/brov2_gps/brov2_gps/gps_data_publisher_node.py
+++ src/brov2_gps/brov2_gps/gps_data_publisher_node.py
@@ -13,21 +13,22 @@ class GPSDataPublisher(Node):
     # Initializer 
     def __init__(self):
         super().__init__('gps_data_publisher')
-        self.declare_parameter('gps_topic_name', 'gps_data')
-        self.declare_parameter('gps_period', 1)
-        self.declare_parameter('serial_connection', '/dev/ttyUSB1')
+        self.declare_parameters(namespace='',
+                                parameters=[('gps_topic_name', 'gps_data'),
+                                            ('gps_period', 1),
+                                            ('serial_connection', '/dev/ttyUSB1')])
 
-        gps_topic_name = self.get_parameter('gps_topic_name').get_parameter_value().string_value
-        gps_period = self.get_parameter('gps_period').get_parameter_value().string_value
-        serial_connection = self.get_parameter('serial_connection').get_parameter_value().string_value
+        gps_topic_name, gps_period, serial_connection = self.get_parameters(['gps_topic_name',
+                                                                             'gps_period',
+                                                                             'serial_connection'])
 
 
-        self.publisher_ = self.create_publisher(NavSatFix, gps_topic_name, 10)
-        self.timer = self.create_timer(gps_period, self.gps_read_and_publish)
+        self.publisher_ = self.create_publisher(NavSatFix, gps_topic_name.value, 10)
+        self.timer = self.create_timer(gps_period.value, self.gps_read_and_publish)
 
         ### GPS related initialization
         # Create a serial connection and GPS module instance.
-        uart = serial.Serial(serial_connection, baudrate=9600, timeout=10)
+        uart = serial.Serial(serial_connection.value, baudrate=9600, timeout=10)
         self.gps = adafruit_gps.GPS(uart, debug=False)
         # Turn on the basic GGA and RMC info (what you typically want)
         self.gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")

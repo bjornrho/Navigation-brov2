3f467dd26a7b0d6c6d3b5214bf37018dfe0c747c
Bjornar <bjornar.rh@gmail.com>
2022-09-07 10:37:18 +0200

    Added description, license and maintainer.

--- src/brov2_trajectory/setup.py
+++ src/brov2_trajectory/setup.py
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='bjornar',
-    maintainer_email='bjornar.rh@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Bjoernar Hogstad',
+    maintainer_email='bjornar-rh@hotmail.com',
+    description='Trajectory publisher for predefined trajectories.',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
